f957eeb46c6198276e023b411626e431d65c54e4
MisreadableMind <ratyshniyv@gmail.com>
2017-09-20 15:27:09 +0300

    fix naming and add rollup for all header menu

--- build-front/page_parts/menu/menu_script.js
+++ build-front/page_parts/menu/menu_script.js
@@ -1,7 +1,7 @@
 'use strict';
 (function ($) {
 	var menuClass = '.menu_nav, .menu_contact'
-		, $navRollups = $('.menu_nav-rollup, .menu_contact-rollup');
+		, $navRollups = $('.menu_nav-rollup, .menu_contacts-rollup');
 
     $(document).ready(function(){
     	$(document).click( function(event){
