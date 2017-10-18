baa2d4211bfd6eb06961dc20c08e5a168c247da8
v.ratyshnyi <ratyshniyv@gmail.com>
2017-10-18 16:42:33 +0300

    Main page: clinics slider

--- build-front/page_parts/carousels/_clinics-carousel.js
+++ build-front/page_parts/carousels/_clinics-carousel.js
@@ -9,7 +9,7 @@
       margin: 0,
       slideBy: 1,
       dots: false,
-      navText: ['&#60;', '&#62;'],
+      navText: ['<i class="icon icon-floating_arrow">', '<i class="icon icon-floating_arrow">'],
       nav: true,
       navContainer: '#clinics_carousel-nav',
       navElement: 'span',
@@ -28,23 +28,6 @@
       var CONSTANT_OWL_INDEX = 2;
       console.log(event);
       var checkLast = event.item.count + event.isTrigger;
-      if (event.item.index === 2) {
-        $('#clinics_carousel-navigation-item-previous').addClass('active');
-        $('#clinics_carousel-navigation-item-next').removeClass('active');
-      } else {
-        $('#clinics_carousel-navigation-item-next').addClass('active');
-        $('#clinics_carousel-navigation-item-previous').removeClass('active');
-      }
-      
-    });
-    
-    $('#clinics_carousel-navigation-item-next').on('click', function () {
-      console.log('this.data.id', $(this).attr('data-id'));
-      clinicsCarousel.trigger('next.owl.carousel');
-    });
-    $('#clinics_carousel-navigation-item-previous').on('click', function () {
-      console.log('this.data.id', $(this).attr('data-id'));
-      clinicsCarousel.trigger('previous.owl.carousel');
     });
   });
   
