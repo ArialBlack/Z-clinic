25de146a2dc57dc0f0879bd6d923345ed7e220db
Vitalii Ratyshnyi <ratyshniyv@gmail.com>
2017-11-19 22:21:46 +0200

    Tabs script and history.

---
+++ build-front/page_parts/tabs/tabs.js
@@ -0,0 +1,10 @@
+'use strict';
+(function ($) {
+  $(document).ready(function(){
+    $('#paragraphID-73 .nav-item .nav-link').removeClass('active');
+    $('#paragraphID-73 .tab-content .tab-pane').removeClass('active');
+  });
+  
+}(jQuery));
+
+
