bb93a81d34835a44a2c7b4651c7a68bcaeae9ae4
Vitalii Ratyshnyi <vitalii@blue-donut.com>
2017-12-08 01:00:01 +0200

    Youtube custom player hotfix.

--- build-front/page_parts/dco_placeholder/dco_placeholder.js
+++ build-front/page_parts/dco_placeholder/dco_placeholder.js
@@ -2,13 +2,11 @@
 (function ($) {
   $(document).ready(function() {
     $('#video_placeholder_btn').on('click', function () {
-      console.log('placeholder works')
       $('.dco_video-banner-placeholder').css('display', 'none');
       $('.dco_video-banner-placeholder-btn').css('display', 'none');
-    });
   
-    $("#video_iframe")[0].src += "&autoplay=1";
-    ev.preventDefault();
+      $("#video_iframe")[0].src += "&autoplay=1";
+    });
   });
   
 }(jQuery));
