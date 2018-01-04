92ae1ba47aa40273518fbdd6b52a5cfe7c5be5bc
Vitalii Ratyshnyi <ratyshniyv@gmail.com>
2018-01-04 14:31:00 +0200

    Fixes for video modals. Hide baby-face-mobile block on desktop.

--- build-front/page_parts/modals/video-modals.js
+++ build-front/page_parts/modals/video-modals.js
@@ -2,6 +2,8 @@
 (function ($) {
   $(".modal.show").on("click", function(e) {
     console.log('e.target', e, $(this));
+    // not working.
+    
     jQuery(".video_modal iframe").attr("src", jQuery(".video_modal iframe").attr("src"));
   });
 }(jQuery));
